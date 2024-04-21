272d36ae1016bd440107ba24c3c3d66ea86bb118
Valery_Boltavsky <valery.boltavsky@checkmarx.com>
2024-04-22 00:09:55 +0300

    api v

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,8 +3,8 @@ import healthRouter from './health';
 import BooksRouter from './books';
 
 const routes = (app: Application) => {
-  app.use('/health', healthRouter);
-  app.use('/books', BooksRouter);
+  app.use('/v1/api/health', healthRouter);
+  app.use('/v1/api/books', BooksRouter);
 };
 
 export default routes;
