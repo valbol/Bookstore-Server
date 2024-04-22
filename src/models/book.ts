b8b0074dff606a026a0779255812fc58d2c52521
Valery_Boltavsky <valery.boltavsky@checkmarx.com>
2024-04-22 22:32:04 +0300

    add required and indexs

--- src/models/book.ts
+++ src/models/book.ts
@@ -3,12 +3,15 @@ import { IBook, Genre } from '../types/book';
 
 const BookSchema = new Schema({
   title: { type: String, required: true },
-  idNumber: { type: String, required: true },
+  idNumber: { type: String, unique:true, required: true },
   description: { type: String, required: true },
   author: { type: String, required: true },
   publicationData: { type: Date, required: true },
   genre: { type: String, enum: Genre, required: true },
   price: { type: Number, required: true },
 });
+
+BookSchema.index({idNumber: 1})
+BookSchema.index({author: 1, genre: 1})
 
 export default mongoose.model<IBook>('Book', BookSchema);
